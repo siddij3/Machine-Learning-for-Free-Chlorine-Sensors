aac013ba754b84e5c71aa275d3c901c5f1f6c9ff
Junaid Siddiqui <junaidas97@gmail.com>
2021-08-30 15:29:47 -0400

    Used arrays to hold multiple data sets

--- plotFit.m
+++ plotFit.m
@@ -1,10 +1,12 @@
-function plotFit(x, y, theta, m)
-
-  plot(x(:, 2), y, 'o', 'MarkerSize', 5, 'LineWidth', 0.75);
+function plotFit(x, y, theta, m, plot_title)
+  
+  figure;
+  plot(x(:, 2), y, 'x', 'MarkerSize', 5, 'LineWidth', 0.75);
   xlabel('Free Chlorine Concentration (ppm) (x)');
   ylabel('Current (µA) (y)');
+  title(plot_title);
   hold on;
-  plot(x(:, 2), x*theta, '--', 'LineWidth', 2)
+  plot(x(:, 2), x*theta, '--', 'LineWidth', 1)
   hold off;
   % ============================================================
 
