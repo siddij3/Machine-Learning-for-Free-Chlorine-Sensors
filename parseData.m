1ca0b88813b7e9897fd1c8b67ffb521665660a90
Junaid Siddiqui <junaidas97@gmail.com>
2021-09-03 12:00:58 -0400

    Extracting fabrication parameters from csv

--- parseData.m
+++ parseData.m
@@ -1,9 +1,17 @@
-function [X, y, t, m] = parseData(all_data, num_iterations)
+function [X, y, params, t, m] = parseData(all_data)
 
   fprintf('Manipulating Data ...\n\n')
-  t = all_data{num_iterations}(2:end, 1);
+
+  params = all_data(1, 1:end) %First row lists all the parameters for the test
+
+
+  % tmp stuff
+
+  all_data = all_data(3:end, :)
+
+  t = all_data(2:end, 1);
   %t(t==0) = [];
-  data = all_data{num_iterations}(:, 2:end)'; %first row is the free chlorine concentration
+  data = all_data(:, 2:end)'; %first row is the free chlorine concentration
   %data(data==0) = [];
   % if the thing has lots of zeroes, then best to remove them somehow
   fcl_conc = data(:, 1);
@@ -14,6 +22,6 @@ function [X, y, t, m] = parseData(all_data, num_iterations)
   X = fcl_conc;
   y = chrono_data;
 
-
+  pause;
 
 end
