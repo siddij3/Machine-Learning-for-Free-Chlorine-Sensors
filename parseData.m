58cf6a4ed004f729e61a40e882dd738a71aea466
Junaid Siddiqui <junaidas97@gmail.com>
2021-09-09 16:51:53 -0400

    Making code more scalable

--- parseData.m
+++ parseData.m
@@ -1,23 +1,23 @@
-function [X, y, params, t, m] = parseData(all_data)
+function [fcl_conc, chrono_data, params, t, m] = parseData(all_data)
 
-  fprintf('Manipulating Data ...\n\n')
+  %First row lists all the parameters for the test
+  params = all_data(1, 1:end);
 
-  params = all_data(1, 1:end); %First row lists all the parameters for the test
-  % tmp stuff
-
+  % Remaining data is the time, free chlorine concetration and current values
   all_data = all_data(3:end, :);
 
+  %time
   t = all_data(2:end, 1);
-  %t(t==0) = [];
-  data = all_data(:, 2:end)'; %first row is the free chlorine concentration
-  %data(data==0) = [];
+
+
+  chrono_data = all_data(:, 2:end);
+
   % if the thing has lots of zeroes, then best to remove them somehow
-  fcl_conc = data(:, 1);
-  chrono_data = data(:, 2:end);
-  m = size(chrono_data)(1); % number of training examples
-  fcl_conc = [ones(m, 1), fcl_conc];
 
-  X = fcl_conc;
-  y = chrono_data;
+  %first row is the free chlorine concentration.
+  %rest of the rows are free chlorine data
+  fcl_conc = chrono_data(1, :);
+  chrono_data = chrono_data(2:end, :);
 
+  m = size(chrono_data)(2); % number of training examples
 end
