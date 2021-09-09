ad0a17eb1e120a748eb4e8e00cbcd50ec7f63775
Junaid Siddiqui <junaidas97@gmail.com>
2021-09-09 16:51:34 -0400

    Renamed file findDerivative

--- findDerivative.m
+++ findDerivative.m
@@ -1,6 +1,6 @@
-function [dydx] = derivative_of_points(x, y)
+function [dydx] = findDerivative(x, y)
   %just finds the derivative of input xs and ys
-  
+
   dydx = diff(y(:))./diff(x(:));
-  
+
 end
