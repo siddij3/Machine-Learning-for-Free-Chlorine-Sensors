3fcc244f8e415d292c25922c821d29aec8988ec0
Junaid Siddiqui <junaidas97@gmail.com>
2021-09-09 20:07:53 -0400

    Plotting as much data as possible

---
+++ plotDataOs.m
@@ -0,0 +1,27 @@
+function plotDataOs(x, y, x_label, y_label, title_label)
+%PLOTDATA Plots the data points x and y into a new figure
+%   PLOTDATA(x,y) plots the data points and gives the figure axes labels of
+%   population and profit.
+
+figure; % open a new figure window
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Plot the training data into a figure using the
+%               "figure" and "plot" commands. Set the axes labels using
+%               the "xlabel" and "ylabel" commands. Assume the
+%               population and revenue data have been passed in
+%               as the x and y arguments of this function.
+%
+% Hint: You can use the 'rx' option with plot to have the markers
+%       appear as red crosses. Furthermore, you can make the
+%       markers larger by using plot(..., 'rx', 'MarkerSize', 10);
+
+
+plot(x, y, 'o', 'MarkerSize', 3, 'LineWidth', 0.75);
+xlabel(x_label);
+ylabel(y_label);
+title(title_label);
+
+% ============================================================
+
+end
