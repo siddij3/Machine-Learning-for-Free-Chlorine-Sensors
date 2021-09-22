61e92a5fb03139ad0108463c901a7d1d3a1b0c6b
Junaid Siddiqui <junaidas97@gmail.com>
2021-09-22 17:10:20 -0400

    Log plot function

---
+++ plotlogPlot.m
@@ -0,0 +1,11 @@
+function plotlogPlot(x, y, x_label, y_label, title_label, position, isNewFig)
+
+loglog(x, y);
+xlabel(x_label);
+ylabel(y_label);
+title(title_label);
+movegui(position);
+
+% ============================================================
+
+end
