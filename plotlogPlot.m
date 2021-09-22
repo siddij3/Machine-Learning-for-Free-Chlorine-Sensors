c3cd3e074a8896a955746edaf9ef119e6623e2bd
Junaid Siddiqui <junaidas97@gmail.com>
2021-09-22 19:01:18 -0400

    Plotting log plots fixed

--- plotlogPlot.m
+++ plotlogPlot.m
@@ -1,11 +1,17 @@
-function plotlogPlot(x, y, x_label, y_label, title_label, position, isNewFig)
+function plotlogPlot(x, y, theta, x_label, y_label, title_label, position, isNewFig)
 
 loglog(x, y);
 xlabel(x_label);
 ylabel(y_label);
 title(title_label);
+%xlim([1E-6 2E-4]);
+%ylim([1E-8 2E-5]);
+xlim([1E-5 2E-4]);
+ylim([5E-8 2E-6]);
 movegui(position);
-
+hold on;
+loglog(x(:, 2), x*theta, '--', 'LineWidth', 1)
+hold off;
 % ============================================================
 
 end
